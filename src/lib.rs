4a7e09595d0578f0d84bef32f4fd12a9af01affb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-09-29 14:15:22 +0200

    Describe what hcube is about

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+#![deny(missing_docs)]
+//! hcube: A Happy Cube generator and solver.
+//!
+//! A [Happy Cube](http://www.happy.be) is a foam puzzle. There are actually two
+//! objectives
+//! 1. Take the six pieces and form them into a cube.
+//! 2. Take the six pieces and put them back in there receptacle.
+//!
+//! hcube offers both style of solvers. Furthermore, it allows one to search the
+//! space for other types puzzles.
+
 #[cfg(test)]
 mod tests {
     #[test]
